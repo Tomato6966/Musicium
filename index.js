f790549772e8b95a21bedd9497eeeba1ee92d29e
Tomato#6966 <68145571+Tomato6966@users.noreply.github.com>
2020-10-29 23:31:25 +0100

    added ping and uptime

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ client.on(`message`, async (message) => {
   if(message.content === `${PREFIX}ping`)
   return message.reply(":ping_pong: `" + client.ws.ping + "ms`")
 
-  if (message.content.toLowerCase() === "//uptime") {
+  if (message.content.toLowerCase() === `${PREFIX}uptime`) {
     let days = Math.floor(client.uptime / 86400000);
     let hours = Math.floor(client.uptime / 3600000) % 24;
     let minutes = Math.floor(client.uptime / 60000) % 60;
