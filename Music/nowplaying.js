d566e708eb36ce890ac012ec1eadaf2a8d3d2bf9
Tomato#6966 <68145571+Tomato6966@users.noreply.github.com>
2020-10-29 17:33:22 +0100

    Fixxed progressbar

--- Music/nowplaying.js
+++ Music/nowplaying.js
@@ -22,38 +22,42 @@ module.exports = {
 execute(message) {
     //if not in a guild return
     if(!message.guild) return;
+    //react with approve emoji
+    message.react(approveemoji)
     //get Server Queue
     const queue = message.client.queue.get(message.guild.id);
     //if nothing playing error
     if (!queue) return attentionembed(message, "There is nothing playing.").catch(console.error);
-    //Define the current song
+    //Define the current song 
     const song = queue.songs[0];
+    //get current song duration in s
+    let minutes = song.duration.split(":")[0];   
+    let seconds = song.duration.split(":")[1];    
+    let ms = (Number(minutes)*60+Number(seconds));   
+    //get thumbnail
+    let thumb;
+    if (song.thumbnail === undefined) thumb = "https://cdn.discordapp.com/attachments/748095614017077318/769672148524335114/unknown.png";
+    else thumb = song.thumbnail.url;
     //define current time
     const seek = (queue.connection.dispatcher.streamTime - queue.connection.dispatcher.pausedTime) / 1000;
     //define left duration
-    const left = song.duration - seek;
-  
+    const left = ms - seek;
     //define embed
     let nowPlaying = new MessageEmbed()
       .setTitle("Now playing")
       .setDescription(`[**${song.title}**](${song.url})`)
-      .setThumbnail("https://cdn.discordapp.com/attachments/748095614017077318/769672148524335114/unknown.png")
+      .setThumbnail(song.thumbnail.url)
       .setColor("#c219d8")
+      .setFooter("Time Remaining: " + new Date(left * 1000).toISOString().substr(11, 8));
       //if its a stream
-      if(song.duration >= 10000) {
+      if(ms >= 10000) {
         nowPlaying.addField("\u200b", "🔴 LIVE", false);
-        nowPlaying.setFooter("Time Remaining: " + new Date(left * 1000).toISOString().substr(11, 8));
-        //react with approve emoji
-        message.react(approveemoji)
         //send approve msg
         return message.channel.send(nowPlaying);
       }
       //If its not a stream 
-      if (song.duration > 0&&song.duration<10000) {
-        nowPlaying.addField("\u200b", new Date(seek * 1000).toISOString().substr(11, 8) + "[" + createBar((song.duration == 0 ? seek : song.duration), seek, 20, "▬", "<:currentposition:770098066552258611>")[0] + "]" + (song.duration == 0 ? " ◉ LIVE" : new Date(song.duration * 1000).toISOString().substr(11, 8)), false);
-        nowPlaying.setFooter("Time Remaining: " + new Date(left * 1000).toISOString().substr(11, 8));
-        //react with approve emoji
-        message.react(approveemoji)
+      if (ms > 0 && ms<10000) {
+        nowPlaying.addField("\u200b", "**[" + createBar((ms == 0 ? seek : ms), seek, 25, "▬", "<:currentposition:770098066552258611>")[0] + "]**\n**" + new Date(seek * 1000).toISOString().substr(11, 8) + " / " + (ms == 0 ? " ◉ LIVE" : new Date(ms * 1000).toISOString().substr(11, 8))+ "**" , false );
         //send approve msg
         return message.channel.send(nowPlaying);
       }
