0b28f70efd6af4f30503102a3c9bb1244789c15d
Tomato6966 <68145571+Tomato6966@users.noreply.github.com>
2021-09-08 11:56:12 +0200

    Add files via upload

---
+++ commands/Info/dashboard.js
@@ -0,0 +1,43 @@
+const {
+  MessageEmbed
+} = require("discord.js");
+const config = require("../../botconfig/config.json");
+var ee = require("../../botconfig/embed.json");
+const settings = require("../../botconfig/settings.json");
+const websiteSettings = require("../../dashboard/settings.json");
+module.exports = {
+  name: "dashboard", //the command name for execution & for helpcmd [OPTIONAL]
+
+  category: "Info",
+  usage: "dashboard",
+  aliases: ["dash"],
+
+  cooldown: 1, //the command cooldown for execution & for helpcmd [OPTIONAL]
+  description: "Sends a Link of the Dashboard", //the command description for helpcmd [OPTIONAL]
+  memberpermissions: [], //Only allow members with specific Permissions to execute a Commmand [OPTIONAL]
+  requiredroles: [], //Only allow specific Users with a Role to execute a Command [OPTIONAL]
+  alloweduserids: [], //Only allow specific Users to execute a Command [OPTIONAL]
+  run: async (client, message, args) => {
+    try {
+      message.reply({
+        embeds: [
+          new MessageEmbed()
+          .setColor(ee.color)
+          .setFooter(ee.footertext, ee.footericon)
+          .setDescription(`> **Website:** ${websiteSettings.website.domain}/\n\n> **Dashboard:** ${websiteSettings.website.domain}/dashboard\n\n> **ServerQueues:** ${websiteSettings.website.domain}/queuedashboard\n\n> **Current Queue:** ${websiteSettings.website.domain}/queue/${message.guild.id}`)
+        ]
+      });
+    } catch (e) {
+      console.log(String(e.stack).bgRed)
+    }
+  }
+}
+/**
+ * @INFO
+ * Bot Coded by Tomato#6966 | https://github.com/Tomato6966/discord-js-lavalink-Music-Bot-erela-js
+ * @INFO
+ * Work for Milrato Development | https://milrato.eu
+ * @INFO
+ * Please mention Him / Milrato Development, when using this Code!
+ * @INFO
+ */
