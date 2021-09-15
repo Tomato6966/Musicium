df3c5b38b5bae38c82f983683231d5f6eb08e22f
Tomato6966 <68145571+Tomato6966@users.noreply.github.com>
2021-09-15 19:32:26 +0200

    Update status.js

--- commands/Queue/status.js
+++ commands/Queue/status.js
@@ -10,11 +10,16 @@ const {
 } = require("../../handlers/functions")
 module.exports = {
 	name: "status", //the command name for the Slash Command
+
+	category: "Queue",
+	aliases: ["stats"],
+	usage: "status",
+
 	description: "Shows the Queue Status", //the command description for Slash Command Overview
 	cooldown: 10,
 	requiredroles: [], //Only allow specific Users with a Role to execute a Command [OPTIONAL]
 	alloweduserids: [], //Only allow specific Users to execute a Command [OPTIONAL]
-	run: async (client, interaction) => {
+	run: async (client, message, args) => {
 		try {
 			//things u can directly access in an interaction!
 			const {
@@ -29,37 +34,36 @@ module.exports = {
 				options,
 				id,
 				createdTimestamp
-			} = interaction;
+			} = message;
 			const {
 				guild
 			} = member;
 			const {
 				channel
 			} = member.voice;
-			if (!channel) return interaction.reply({
+			if (!channel) return message.reply({
 				embeds: [
 					new MessageEmbed().setColor(ee.wrongcolor).setTitle(`${client.allEmojis.x} **Please join ${guild.me.voice.channel ? "__my__" : "a"} VoiceChannel First!**`)
 				],
-				ephemeral: true
+
 			})
 			if (channel.guild.me.voice.channel && channel.guild.me.voice.channel.id != channel.id) {
-				return interaction.reply({
+				return message.reply({
 					embeds: [new MessageEmbed()
 						.setColor(ee.wrongcolor)
 						.setFooter(ee.footertext, ee.footericon)
 						.setTitle(`${client.allEmojis.x} Join __my__ Voice Channel!`)
 						.setDescription(`<#${guild.me.voice.channel.id}>`)
 					],
-					ephemeral: true
 				});
 			}
 			try {
 				let newQueue = client.distube.getQueue(guildId);
-				if (!newQueue || !newQueue.songs || newQueue.songs.length == 0) return interaction.reply({
+				if (!newQueue || !newQueue.songs || newQueue.songs.length == 0) return message.reply({
 					embeds: [
 						new MessageEmbed().setColor(ee.wrongcolor).setTitle(`${client.allEmojis.x} **I am nothing Playing right now!**`)
 					],
-					ephemeral: true
+
 				})
 				var djs = client.settings.get(newQueue.id, `djroles`);
 				if(!djs || !Array.isArray(djs)) djs = [];
@@ -83,18 +87,18 @@ module.exports = {
 					.setFooter(`💯 ${newTrack.user.tag}`, newTrack.user.displayAvatarURL({
 						dynamic: true
 					}));
-				interaction.reply({
+				message.reply({
 					embeds: [embed]
 				})
 			} catch (e) {
 				console.log(e.stack ? e.stack : e)
-				interaction.editReply({
+				message.reply({
 					content: `${client.allEmojis.x} | Error: `,
 					embeds: [
 						new MessageEmbed().setColor(ee.wrongcolor)
 						.setDescription(`\`\`\`${e}\`\`\``)
 					],
-					ephemeral: true
+
 				})
 			}
 		} catch (e) {
